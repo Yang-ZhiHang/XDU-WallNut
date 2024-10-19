af058b7ddd1167caf6f50448184175d7874dd89f
Yang-ZhiHang <2097853195@qq.com>
2024-10-19 15:56:24 +0800

    fix: 修复了一些小问题

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20f21f5ea47f4ea2c55b35a2845f39da461ea730e1cac20f3c6ac729ebdd9c82
-size 13998
+oid sha256:dd9312062b4af50d9b61de557892eb7739ae36b83b5609b3a04191704c1144b3
+size 13864
