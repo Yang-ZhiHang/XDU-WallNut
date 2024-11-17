ac80e40c70a87ec88a1ac7b994924053866d6c89
Yang-ZhiHang <2097853195@qq.com>
2024-11-17 23:28:57 +0800

    release-v1.2.2

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from utils.browser_utils import open_website
 from utils.config_handler import ConfigHandler
 from ui.base_window import BaseWindow
 from ui.components.progress_handler import ProgressHandler
+from utils.style_handler import StyleManager
 import ctypes
+
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
+
 
 class XDUScript(BaseWindow):
     def __init__(self):
@@ -20,6 +23,9 @@ class XDUScript(BaseWindow):
         # 连接开始按钮的点击事件
         self.start_button.clicked.connect(self.config_handler.get_config)
 
+        # 样式处理器属性
+        self.style_handler = None
+
         # 初始化时打开网页
         open_website(
             "https://ehall.xidian.edu.cn/jwapp/sys/wspjyyapp/*default/index.do",
@@ -30,5 +36,10 @@ class XDUScript(BaseWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = XDUScript()
+
+    # 加载样式
+    ex.style_handler = StyleManager(ex, app)
+    ex.style_handler.apply_styles()
+
     ex.show()
     sys.exit(app.exec_())
