2e22d37c8bcae60468d7746540c69b98c1884eac
Yang-ZhiHang <2097853195@qq.com>
2024-12-14 00:46:08 +0800

    feat: 添加了没用的设置页

--- src/ui/widgets/console_widget.py
+++ src/ui/widgets/console_widget.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:325211ea864adc905d053e6b41b5c06008269a3a3e96f3d403819e514614d977
-size 457
+oid sha256:68dc44206bbaf724afe2636ff2fa107fb8e92ebf9c7655c76d35e1610e44e0c4
+size 464
--- src/ui/windows/main_window.py
+++ src/ui/windows/main_window.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ebb2ec2b321e05ef598c2fe2e19ba5f7f396a668c5d71dee97b203ac9fd79d
-size 8112
+oid sha256:ee3895f05cf3ad44731666f3cbab56396c05c25dcf0cb9862e5d0130635fbb25
+size 8956
