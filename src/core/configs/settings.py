c0487f3911f2ca98a22e100b31c9e83d6bb7728a
Yang-ZhiHang <2097853195@qq.com>
2024-12-14 01:30:32 +0800

    feat: 添加了获取更新的逻辑

--- src/core/configs/settings.py
+++ src/core/configs/settings.py
@@ -5,3 +5,7 @@ class Settings:
     # 窗口相关
     WINDOW_TITLE = "XDU一键评教"
     WINDOW_SIZE = (400, 600)
+
+    # 更新相关
+    # UPDATE_URL = "https://github.com/Yang-ZhiHang/XDU-WallNut"
+    UPDATE_URL = "https://api.github.com/repos/Yang-ZhiHang/XDU-WallNut/releases/latest"
