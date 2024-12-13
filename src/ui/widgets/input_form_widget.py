0efc258fc2ca86ffb3fa31ca9ed1467c0cd55f50
Yang-ZhiHang <2097853195@qq.com>
2024-12-14 00:11:38 +0800

    feat: 完成了增强模式页的功能设计

--- src/ui/widgets/input_form_widget.py
+++ src/ui/widgets/input_form_widget.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb11a31dcf21e03eb0e4e1a0024046f7d7ec0be49839571d16eb36b4962e38cc
-size 4323
+oid sha256:89bc6f6ce69394231ad050b75a46194665417a29ff990bce1f2f0f3a7239e9cf
+size 5573
--- src/ui/windows/main_window.py
+++ src/ui/windows/main_window.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1d4334fe8e7dea1d6fde5a77d702d3c02748088e4b9ce454fdbfc0cc909dace
-size 4570
+oid sha256:c0ebb2ec2b321e05ef598c2fe2e19ba5f7f396a668c5d71dee97b203ac9fd79d
+size 8112
