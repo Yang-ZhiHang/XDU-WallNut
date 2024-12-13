b2ec10146643eebbc6db7da81f0ca5852cdf1c7d
Yang-ZhiHang <2097853195@qq.com>
2024-12-13 19:04:24 +0800

    feat: 添加了应用内图标

--- src/test/src/ui/windows/main_window.py
+++ src/test/src/ui/windows/main_window.py
@@ -3,15 +3,17 @@
 """
 import os
 import sys
-
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+
 try:
-    # 导入项目根目录到 Python 路径
     sys.path.append(
         os.path.abspath(os.path.join(os.path.dirname(__file__), "./../../"))
     )
     from core.configs.constants import COMMENTS_OPTIONS, OPTION_LABELS
     from core.configs.settings import Settings
+    from core.icons import Icon
+    from utils.logger import Logger
     from core.loaders.style_loader import StyleLoader
     from core.loaders.web_loader import WebLoader
     from ui.widgets.console_widget import ConsoleOutput
@@ -36,23 +38,26 @@ class MainWindow(QMainWindow):
         # 设置窗口大小
         self.resize(*Settings.WINDOW_SIZE)
 
+        # 设置窗口图标
+
+        # 检查资源是否存在
+        if os.path.exists(Icon.logo_ico_path):
+            Logger.info("main_window.py", "set_window_icon", "Found icon resource.", color="green")
+            icon = QIcon(Icon.logo_ico_path)
+        else:
+            Logger.error("main_window.py", "set_window_icon", "Can't find icon resource.")
+        self.setWindowIcon(icon)
+
         # 创建中心部件并创建主布局
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.main_layout = QVBoxLayout(self.central_widget)
 
-        # 输入表单
         self.input_form_choices = InputForm("是否评教选择题：", OPTION_LABELS)
         self.input_form_text = InputForm("是否评教文本框：", COMMENTS_OPTIONS)
-
-        # 样式
         self.style_loader = StyleLoader("base.qss")
-
-        # 控制台输出
-        self.console_output = ConsoleOutput()
-
-        # 浏览器
         self.web_loader = WebLoader()
+        self.console_output = ConsoleOutput()
 
     def _apply_component(self):
         """应用所有初始化过的组件"""
