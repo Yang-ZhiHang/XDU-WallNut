69badc1b48537e15484ac97a8806607d3e7b88de
Yang-ZhiHang <2097853195@qq.com>
2024-12-13 20:11:43 +0800

    feat: 添加了增强模式分页

--- src/core/configs/settings.py
+++ src/core/configs/settings.py
@@ -4,4 +4,4 @@
 class Settings:
     # 窗口相关
     WINDOW_TITLE = "XDU一键评教"
-    WINDOW_SIZE = (800, 600)
+    WINDOW_SIZE = (400, 600)
--- src/ui/widgets/start_button_widget.py
+++ src/ui/widgets/start_button_widget.py
@@ -5,35 +5,34 @@
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtCore import pyqtSignal
 
+
 class StartButton(QPushButton):
     # 定义信号
     started = pyqtSignal()  # 开始信号
     stopped = pyqtSignal()  # 停止信号
-    
+
     def __init__(self):
         super().__init__()
         self._running = False
         self._init_ui()
-        
+
     def _init_ui(self):
-        self.setText("开始")
+        self.setText("开始运行")
         self.setMinimumWidth(80)  # 设置最小宽度
         self.clicked.connect(self._on_click)
-        
+
     def _on_click(self):
         """点击按钮时，切换按钮状态"""
         if not self._running:
-            self.setText("停止")
+            self.setText("停止运行")
             self._running = True
             self.started.emit()
         else:
-            self.setText("开始")
+            self.setText("开始运行")
             self._running = False
             self.stopped.emit()
-            
+
     def reset(self):
         """重置按钮状态"""
         self._running = False
-        self.setText("开始")
-
-    
+        self.setText("开始运行")
