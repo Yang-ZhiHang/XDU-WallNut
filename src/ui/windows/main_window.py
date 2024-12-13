8316746f1a256e5b5d2a3c7b4e7b9b47bb4fa10c
Yang-ZhiHang <2097853195@qq.com>
2024-12-13 20:33:54 +0800

    chore: 修改了 main window 的逻辑

--- src/ui/windows/main_window.py
+++ src/ui/windows/main_window.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22916686cd90aa9a697ddd420c193e4db9ad6b44df0be470e0dde46b39cae928
-size 3545
+oid sha256:0d56642c660c9b9a558aad4a81147c1e68dcd8dadc3c7207b8bb4f8ce1a56e35
+size 4154
