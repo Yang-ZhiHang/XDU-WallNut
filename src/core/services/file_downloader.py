b62ddc4e25c382eb71cf197e9755e089afc1a7dc
Yang-ZhiHang <2097853195@qq.com>
2024-12-17 18:54:32 +0800

    chore: 修改彩蛋文字

--- src/core/services/file_downloader.py
+++ src/core/services/file_downloader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:788706cf9bbd5be24170006544cfc686ab28fb615d951639d821db1916760f41
+oid sha256:87e15b78a2a949149005160eaebbd83fec37b68ca738ee9699d245f4d28eac65
 size 4139
