c1c97c6a563e9e74256f4bc25a73075d549f0434
Yang-ZhiHang <2097853195@qq.com>
2024-12-17 15:22:39 +0800

    feat: 添加了窗口置顶功能

--- src/ui/windows/main_window.py
+++ src/ui/windows/main_window.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d95112bc938709be786ddeca391c4f14ea6e0d3a84efe9b905fbc9406a24f40
-size 11556
+oid sha256:d8b663d47b68078bd8c9dac279ad45c03bc7ec6f584920e8013229643305aa71
+size 11948
